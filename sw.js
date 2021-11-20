e5f1711cfe282e9eea351cd8990d09784a1ccef0
Babatunde Fatai <33842571+Babatunde-Fatai@users.noreply.github.com>
2021-11-20 11:23:49 +0100

    update

--- sw.js
+++ sw.js
@@ -68,33 +68,70 @@ self.addEventListener('install', function(event) {
 
 
 //whenever we have a fetch request
+// self.addEventListener('fetch', function(event) {
+//     event.respondWith(
+//       caches.match(event.request)
+//         .then(function(response) {
+//           // Cache hit - return response
+//           if (response) {
+//             return response;
+//           }
+//           return fetch(event.request);
+//         }
+//       )
+//     );
+//   });
+
 self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request)
-        .then(function(response) {
-          // Cache hit - return response
-          if (response) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
             return response;
           }
-          return fetch(event.request);
-        }
-      )
+        );
+      })
     );
-  });
+});
 
-  // self.addEventListener('activate', function(event) {
-  //   event.waitUntil(
-  //     // Get all the cache names
-  //     caches.keys().then(function(cacheNames) {
-  //       return Promise.all(
-  //         // Get all the items that are stored under a different cache name than the current one
-  //         cacheNames.filter(function(cacheName) {
-  //           return cacheName != currentCacheName;
-  //         }).map(function(cacheName) {
-  //           // Delete the items
-  //           return caches.delete(cacheName);
-  //         })
-  //       ); // end Promise.all()
-  //     }) // end caches.keys()
-  //   ); // end event.waitUntil()
-  // });
+
+//looping through all of the caches in the service 
+//worker and deleting any caches that aren't defined in the cache allowlist.
+self.addEventListener('activate', function(event) {
+
+  var cacheAllowlist = ['maxa-cache-v1']; //list all cache
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheAllowlist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
